8fe5e1a71fa92789b74f6dbc68b0f9efc56a53e0
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-06-22 00:17:56 -0300

    Corrigindo estrutura de arquivos p java n reclamar dos namespaces

--- src/test/java/calculadora/CalculadoraTest.java
+++ src/test/java/calculadora/CalculadoraTest.java
@@ -1,6 +1,5 @@
 package calculadora;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -13,41 +12,40 @@ import org.junit.jupiter.api.Test;
 
 @DisplayName("Classe para teste da calculadora")
 public class CalculadoraTest {
-	
+
 	private Calculadora calc;
-	
+
 	@BeforeEach
 	public void inicializa() {
 		calc = new Calculadora();
 	}
-	
-	@DisplayName("Testa a soma de dois números")
+
+	@DisplayName("Testa a soma de dois nï¿½meros")
 	@Test
 	public void testSomaDoisNumeros() {
-		int soma = calc.soma(4, 5);		
-		Assertions.assertEquals(9, soma);		
+		int soma = calc.soma(4, 5);
+		Assertions.assertEquals(9, soma);
 	}
-	
+
 	@Test
 	public void testDivisaoDoisNumeros() {
 		int divisao = calc.divisao(8, 4);
 		assertTrue(divisao == 2);
 	}
-	
+
 	@Test
 	public void testDivisaoPorZero() {
 		try {
 			int divisao = calc.divisao(8, 0);
-			fail("Exceção não lançada");
-		}catch (ArithmeticException e) {
+			fail("Exceï¿½ï¿½o nï¿½o lanï¿½ada");
+		} catch (ArithmeticException e) {
 			assertEquals("/ by zero", e.getMessage());
-		}		
+		}
 	}
-	
+
 	@Test
 	public void testDivisaoPorZeroComAssertThrows() {
-		assertThrows(ArithmeticException.class,
-				() -> calc.divisao(8, 0));
+		assertThrows(ArithmeticException.class, () -> calc.divisao(8, 0));
 	}
 
 }
